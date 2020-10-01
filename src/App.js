9011170e57f8c156be11d7ff245cc8373926aa31
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-01 23:55:31 +0300

    activate useState hook commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react'
-import TodoList from './TodoList'
+import React, {useState} from 'react';
+import TodoList from './TodoList';
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
-  }
+export default function App() {
+  // state = {
+  //   todos: [
+  //     {id: 1, title: 'First todo', completed: false},
+  //     {id: 2, title: 'Second todo', completed: true}
+  //   ]
+  // }
+  const [todos, setTodos] = useState( [
+    {id: 1, title: 'First todo', completed: false},
+    {id: 2, title: 'Second todo', completed: true}
+  ]);
 
-  render() {
-    return (
-      <div className="container">
-        <h1>Todo app</h1>
+  return (
+    <div className="container">
+      <h1>Todo app</h1>
 
-          <div className="input-field">
-            <input type="text" />
-            <label>Todo name</label>
-          </div>
+        <div className="input-field">
+          <input type="text" />
+          <label>Todo name</label>
+        </div>
 
-          <TodoList todos={this.state.todos} />
-      </div>
-    );
-  }
+        <TodoList todos={todos} />
+    </div>
+  );
+  
 }
